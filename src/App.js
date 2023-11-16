0cc7a0c0056e3651d841fbf1a70301b1451ccc4d
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-11-16 17:47:16 +0900

    remove privacyPolicy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
-import { PrivacyPolicy } from "./components/PrivacyPolicy";
 
 function App() {
   return (
@@ -8,11 +7,8 @@ function App() {
       <div className="App">
         <Link to="/">Home</Link>
         <br />
-        <Link to="/PrivacyPolicy">PrivacyPolicy</Link>
-        <br />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
         </Routes>
       </div>
     </BrowserRouter>
