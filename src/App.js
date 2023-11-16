3c694e005ad1591e61759144df48bbec16a15178
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-11-16 14:03:05 +0900

    プライバシーポリシーページ追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+// react-router-domのインポートを追加
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { Home } from "./components/Home";
+import { PrivacyPolicy } from "./components/PrivacyPolicy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/PrivacyPolicy">PrivacyPolicy</Link>
+        <br />
+
+        <Routes>
+          {/* exactをつけると完全一致になります。Homeはexactをつけてあげます */}
+          <Route exact path="/" element={<Home />} />
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
